3917ae7c52383ea3dcba90ad207cfe027304631e
AndyBond <79160937104@yandex.ru>
2025-09-11 18:52:50 +0000

    style: PHP styler apply

--- otus/debug.php
+++ otus/debug.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 
-/**
- * @var CMain $Application
- */
 $APPLICATION->SetTitle('Домашнее задание - Отладка и тестирование');
 
-echo '<h1>Часть 1</h1>';
+/** @var int[] Массив списка по первой части д/з */
 $links = array(
-array("Открыть страницу","/otus/simplelog.php"),
-array("Журнал","../simplelog.log")
+    array("Открыть страницу", "/otus/simplelog.php"),
+    array("Журнал", "../simplelog.log")
 );
+
+echo '<h1>Часть 1</h1>';
 echo '<ul>';
-foreach ($links as $urlitem){ 
-echo "<li><a href='".$urlitem[1]."'>".$urlitem[0]."</a></li>";
+
+foreach ($links as $urlitem) {
+    echo "<li><a href='" . $urlitem[1] . "'>" . $urlitem[0] . "</a></li>";
 }
+
 echo '</ul';
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
-?>
--- otus/simplelog.php
+++ otus/simplelog.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 
 use Bitrix\Main\Diag\Debug;
@@ -8,11 +8,11 @@ $APPLICATION->SetTitle('Домашнее задание - Отладка и те
 echo '<h1>Часть 1</h1>';
 echo '<h2>Простое логгирование</h2>';
 
+/** @var string Сформированная трока для записи в журнал и вывода на экран */
 $current_datetime = 'Страница открыта: ' . date("Y-m-d H:i:s");
 
 echo $current_datetime;
 
-Debug::writeToFile( $current_datetime, '', './simplelog.log' );
+Debug::writeToFile($current_datetime, '', './simplelog.log');
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
-?>
